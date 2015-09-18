94bd694de0da713e351e1dd687de4c2616391950
Egbert Bouman <ebouman@gmail.com>
2015-09-19 01:52:49 +0200

    Import initial code

---
+++ js/playlist_builder.js
@@ -0,0 +1,105 @@
+playlist_builder = {};
+
+(function(playlist_builder, $) {
+
+    playlist_builder.jamendo_client = '9d9f42e3';
+    playlist_builder.search_results = {};
+
+    playlist_builder.create = function() {
+        this.playlist = new jPlayerPlaylist({
+          jPlayer: '#player-core',
+          cssSelectorAncestor: '#player-ui'
+        }, [
+          {
+            title:"Title",
+            artist:"Artist",
+            mp3:'http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3',
+            oga:'http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.ogg',
+            poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+          },
+          {
+            title:"Title2",
+            artist:"Artist2",
+            mp3:'http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.mp3',
+            oga:'http://www.jplayer.org/audio/mp3/Miaow-07-Bubble.ogg',
+            poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+          }
+        ],
+        {
+          swfPath: 'js/jplayer-2.2.0',
+          supplied: 'mp3,m4a',
+          wmode: 'window'
+        });
+    }
+
+    playlist_builder.search = function() {
+        var self = this;
+        var query = $("#search-query").val();
+        var results = $("#search-results");
+        $.getJSON("https://api.jamendo.com/v3.0/tracks/?client_id=" + this.jamendo_client + "&format=jsonpretty&limit=20&namesearch=" + query + "&groupby=artist_id", function(data) {
+            //TODO: check success
+            results.empty();
+            $.each(data['results'], function(key, val) {
+
+                self.search_results[val['id']] = {
+                    title: val['name'],
+                    artist: val['artist_name'],
+                    mp3: val['audio'],
+                    poster: val['image']
+                };
+
+                var item_html = '<li class="list-group-item shorten">';
+
+                item_html += '<div class="pull-right m-l btn-group">';
+                item_html += '<a href="#" onclick="playlist_builder.play(' + val['id'] + '); return false;" class="m-r-sm btn-play"><span class="glyphicon glyphicon-play text-success"></span></a>';
+                item_html += '<a href="#" onclick="playlist_builder.add(' + val['id'] + '); return false;" class="m-r-sm btn-play"><span class="glyphicon glyphicon-plus text-primary"></span></a>';
+                item_html += '</div>';
+
+                item_html += '<img src="' + val['image'] + '" alt="" class="img-thumbnail covert-art"';
+                item_html += '<a href="javascript:;" class="jp-playlist-item" tabindex="0">' + val['name'] + ' <span class="jp-artist">' + val['artist_name'] + '</span></a>';
+
+                item_html += '</li>';
+
+                $(item_html).appendTo(results);
+            });
+        });
+    }
+
+    playlist_builder.add = function(jamendo_id) {
+        if (jamendo_id in this.search_results) {
+            this.playlist.add(this.search_results[jamendo_id]);
+        }
+        else {
+            var self = this;
+            $.getJSON("https://api.jamendo.com/v3.0/tracks/?client_id=" + self.jamendo_client + "&id=" + jamendo_id, function(data) {
+                //TODO: check success
+                $.each(data['results'], function(key, val) {
+
+                    self.playlist.add({
+                        title: val['name'],
+                        artist: val['artist_name'],
+                        mp3: val['audio'],
+                        poster: val['image']
+                    });
+
+                });
+            });   
+        }
+    }
+
+    playlist_builder.play = function(jamendo_id) {
+        if (jamendo_id in this.search_results) {
+            $(this.playlist.cssSelector.jPlayer).jPlayer("setMedia", {mp3: this.search_results[jamendo_id]['mp3']}).jPlayer("play");
+        }
+        else {
+            var self = this;
+            $.getJSON("https://api.jamendo.com/v3.0/tracks/?client_id=" + self.jamendo_client + "&id=" + jamendo_id, function(data) {
+                //TODO: check success
+                $(self.playlist.cssSelector.jPlayer).jPlayer("setMedia", {mp3: data['results'][0]['audio']}).jPlayer("play");
+            });   
+        }
+    }
+
+})(playlist_builder, jQuery);
+
+
